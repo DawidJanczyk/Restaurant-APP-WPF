f1b3b0b7f4048706517705261490d13fe13378d2
Dawid Janczyk <45544015+DawidJanczyk@users.noreply.github.com>
2019-04-12 22:47:22 +0200

    Add files via upload

---
+++ Main Window/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Restaurant_APP
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MouseEnterMenu(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 1;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(50));
+            RectangleMenu.BeginAnimation(OpacityProperty, animation);
+        }
+
+        private void MouseLeaveMenu(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 1;
+            animation.To = 0;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(50));
+            RectangleMenu.BeginAnimation(OpacityProperty, animation);
+        }
+
+        private void MouseEnterAbout(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 1;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(50));
+            RectangleAbout.BeginAnimation(OpacityProperty, animation);
+            RectangleAbout1.BeginAnimation(OpacityProperty, animation);
+            textBlockAbout.BeginAnimation(OpacityProperty, animation);
+        }
+
+        private void MouseLeaveAbout(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 1;
+            animation.To = 0;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(50));
+            RectangleAbout.BeginAnimation(OpacityProperty, animation);
+
+            DoubleAnimation animation1 = new DoubleAnimation();
+            animation1.From = 0;
+            animation1.To = 0;
+            animation1.Duration = new Duration(TimeSpan.FromMilliseconds(1));
+            RectangleAbout1.BeginAnimation(OpacityProperty, animation1);
+            textBlockAbout.BeginAnimation(OpacityProperty, animation1);
+
+        }
+
+        private void MouseEnterContact(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 1;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(50));
+            RectangleContact.BeginAnimation(OpacityProperty, animation);
+        }
+
+        private void MouseLeaveContact(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 1;
+            animation.To = 0;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(50));
+            RectangleContact.BeginAnimation(OpacityProperty, animation);
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            Menu use = new Menu();
+            use.Show();
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void ContactButton_Click(object sender, RoutedEventArgs e)
+        {
+            Contact use = new Contact();
+            use.Show();
+        }
+    }
+}
---
+++ Menu/Menu.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Restaurant_APP
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAM_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 1;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(100));
+            RectangleBreakfast.BeginAnimation(OpacityProperty, animation);
+
+            DoubleAnimation animation1 = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 0;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(100));
+            RectangleDinner.BeginAnimation(OpacityProperty, animation);
+
+        }
+
+        private void ButtonPM_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 1;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(100));
+            RectangleDinner.BeginAnimation(OpacityProperty, animation);
+
+            DoubleAnimation animation1 = new DoubleAnimation();
+            animation.From = 0;
+            animation.To = 0;
+            animation.Duration = new Duration(TimeSpan.FromMilliseconds(100));
+            RectangleBreakfast.BeginAnimation(OpacityProperty, animation);
+
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void ButtonLeft_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow use = new MainWindow();
+            use.Show();
+            this.Close();
+        }
+    }
+}
